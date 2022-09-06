e45b2b78b03bf862117f6e11b2473a2e500464dd
Alex Titarenko <alx.titarenko@gmail.com>
2022-09-05 20:35:44 -0700

    remove unused params

--- src/storage/readPackIndex.ts
+++ src/storage/readPackIndex.ts
@@ -7,45 +7,25 @@ type LoadPackIndexParams = {
   fs: FileSystem
   filename: string
   getExternalRefDelta: (oid: string) => Promise<{ type: string, object: Buffer }>
-  emitter?: any
-  emitterPrefix?: any
 }
 
 type ReadPackIndexParams = LoadPackIndexParams & {
   cache: Cache
 }
 
-async function loadPackIndex({
-  fs,
-  filename,
-  getExternalRefDelta,
-  emitter,
-  emitterPrefix,
-}: LoadPackIndexParams) {
+async function loadPackIndex({ fs, filename, getExternalRefDelta }: LoadPackIndexParams) {
   const idx = (await fs.read(filename) as Buffer)
   return GitPackIndex.fromIdx({ idx, getExternalRefDelta })
 }
 
-export function readPackIndex({
-  fs,
-  cache,
-  filename,
-  getExternalRefDelta,
-  emitter,
-  emitterPrefix,
-}: ReadPackIndexParams) {
+export function readPackIndex({ fs, cache, filename, getExternalRefDelta }: ReadPackIndexParams) {
   // Try to get the packfile index from the in-memory cache
   if (!cache[PackfileCache]) cache[PackfileCache] = new Map<string, Promise<GitPackIndex>>()
   let p = cache[PackfileCache].get(filename)
   if (!p) {
-    p = loadPackIndex({
-      fs,
-      filename,
-      getExternalRefDelta,
-      emitter,
-      emitterPrefix,
-    })
+    p = loadPackIndex({ fs, filename, getExternalRefDelta })
     cache[PackfileCache].set(filename, p)
   }
+
   return p
 }
