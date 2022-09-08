b2fb28fad3682367ac95d23df2424c7c4641b8e5
Alex Titarenko <alx.titarenko@gmail.com>
2022-09-08 16:13:07 -0700

    add checkout api

---
+++ src/api/checkout.ts
@@ -0,0 +1,114 @@
+import { Cache } from '../models/Cache'
+import { _checkout } from '../commands/checkout'
+import { FileSystem } from '../models/FileSystem'
+import { assertParameter } from '../utils/assertParameter'
+import { join } from '../utils/join'
+import { IBackend } from '../models/IBackend'
+import { ProgressCallback } from '../models'
+
+type CheckoutParams = {
+  fs: IBackend
+  onProgress: ProgressCallback
+  dir: string
+  gitdir?: string
+  remote?: string
+  ref?: string
+  filepaths: string[]
+  noCheckout?: boolean
+  noUpdateHead?: boolean
+  dryRun?: boolean
+  force?: boolean
+  cache?: Cache
+}
+
+/**
+ * Checkout a branch
+ *
+ * If the branch already exists it will check out that branch. Otherwise, it will create a new remote tracking branch set to track the remote branch of that name.
+ *
+ * @param {object} args
+ * @param {FsClient} args.fs - a file system implementation
+ * @param {ProgressCallback} [args.onProgress] - optional progress event callback
+ * @param {string} args.dir - The [working tree](dir-vs-gitdir.md) directory path
+ * @param {string} [args.gitdir=join(dir,'.git')] - [required] The [git directory](dir-vs-gitdir.md) path
+ * @param {string} [args.ref = 'HEAD'] - Source to checkout files from
+ * @param {string[]} [args.filepaths] - Limit the checkout to the given files and directories
+ * @param {string} [args.remote = 'origin'] - Which remote repository to use
+ * @param {boolean} [args.noCheckout = false] - If true, will update HEAD but won't update the working directory
+ * @param {boolean} [args.noUpdateHead] - If true, will update the working directory but won't update HEAD. Defaults to `false` when `ref` is provided, and `true` if `ref` is not provided.
+ * @param {boolean} [args.dryRun = false] - If true, simulates a checkout so you can test whether it would succeed.
+ * @param {boolean} [args.force = false] - If true, conflicts will be ignored and files will be overwritten regardless of local changes.
+ * @param {object} [args.cache] - a [cache](cache.md) object
+ *
+ * @returns {Promise<void>} Resolves successfully when filesystem operations are complete
+ *
+ * @example
+ * // switch to the main branch
+ * await git.checkout({
+ *   fs,
+ *   dir: '/tutorial',
+ *   ref: 'main'
+ * })
+ * console.log('done')
+ *
+ * @example
+ * // restore the 'docs' and 'src/docs' folders to the way they were, overwriting any changes
+ * await git.checkout({
+ *   fs,
+ *   dir: '/tutorial',
+ *   force: true,
+ *   filepaths: ['docs', 'src/docs']
+ * })
+ * console.log('done')
+ *
+ * @example
+ * // restore the 'docs' and 'src/docs' folders to the way they are in the 'develop' branch, overwriting any changes
+ * await git.checkout({
+ *   fs,
+ *   dir: '/tutorial',
+ *   ref: 'develop',
+ *   noUpdateHead: true,
+ *   force: true,
+ *   filepaths: ['docs', 'src/docs']
+ * })
+ * console.log('done')
+ */
+export async function checkout({
+  fs,
+  onProgress,
+  dir,
+  gitdir = join(dir, '.git'),
+  remote = 'origin',
+  ref: _ref,
+  filepaths,
+  noCheckout = false,
+  noUpdateHead = _ref === undefined,
+  dryRun = false,
+  force = false,
+  cache = {},
+}: CheckoutParams): Promise<void> {
+  try {
+    assertParameter('fs', fs)
+    assertParameter('dir', dir)
+    assertParameter('gitdir', gitdir)
+
+    const ref = _ref || 'HEAD'
+    return await _checkout({
+      fs: new FileSystem(fs),
+      cache,
+      onProgress,
+      dir,
+      gitdir,
+      remote,
+      ref,
+      filepaths,
+      noCheckout,
+      noUpdateHead,
+      dryRun,
+      force,
+    })
+  } catch (err: any) {
+    err.caller = 'git.checkout'
+    throw err
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export * as Errors from './errors/index'
 
 export { add } from './api/add'
 export { addRemote } from './api/addRemote'
-// export { checkout } from './api/checkout'
+export { checkout } from './api/checkout'
 // export { clone } from './api/clone'
 export { commit } from './api/commit'
 export { currentBranch } from './api/currentBranch'
