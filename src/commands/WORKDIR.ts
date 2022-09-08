ad30d5df7dfca1aca5d298c33e74c0bf290ce03b
Alex Titarenko <alx.titarenko@gmail.com>
2022-09-07 17:04:49 -0700

    add WORKDIR command

---
+++ src/commands/WORKDIR.ts
@@ -0,0 +1,20 @@
+import { FileSystem } from '../models/FileSystem'
+import { Cache } from '../models/Cache'
+import { GitWalkerFs } from '../models/GitWalkerFs'
+import { GitWalkSymbol, Walker } from '../models/Walker'
+
+/**
+ * @returns {Walker}
+ */
+export function WORKDIR(): Walker {
+  const o = Object.create(null)
+  Object.defineProperty(o, GitWalkSymbol, {
+    value: function(
+      { fs, dir, gitdir, cache }:
+      { fs: FileSystem, dir: string, gitdir: string, cache: Cache }) {
+      return new GitWalkerFs({ fs, dir, gitdir, cache })
+    },
+  })
+  Object.freeze(o)
+  return o
+}
