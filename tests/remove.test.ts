3e40e2958fd667f5d602e9179a020b008c85d17b
Alex Titarenko <alx.titarenko@gmail.com>
2022-09-16 06:08:21 -0700

    add unit tests for remove api

---
+++ tests/remove.test.ts
@@ -0,0 +1,142 @@
+import { expect } from 'chai'
+
+import { remove, listFiles } from '../src'
+import { makeFsFixture, DataFixture } from './helpers/makeFsFixture'
+
+import remoteDataFixture from './fixtures/data/remove.json'
+
+describe('remove', () => {
+  it('file', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture(remoteDataFixture as DataFixture)
+
+    // act
+    const before = await listFiles({ fs, dir })
+
+    // assert
+    expect(before).to.have.members([
+      ".babelrc",
+      ".editorconfig",
+      ".flowconfig",
+      ".gitignore",
+      ".travis.yml",
+      "LICENSE.md",
+      "package-lock.json",
+      "package.json",
+      "shrinkwrap.yaml",
+      "src/commands/checkout.js",
+      "src/commands/config.js",
+      "src/commands/fetch.js",
+      "src/commands/init.js",
+      "src/index.js",
+      "src/models/GitBlob.js",
+      "src/models/GitCommit.js",
+      "src/models/GitConfig.js",
+      "src/models/GitObject.js",
+      "src/models/GitTree.js",
+      "src/utils/exists.js",
+      "src/utils/mkdirs.js",
+      "src/utils/read.js",
+      "src/utils/resolveRef.js",
+      "src/utils/write.js",
+    ])
+
+    // act
+    await remove({ fs, dir, filepath: 'LICENSE.md' })
+    const after = await listFiles({ fs, dir })
+
+    // assert
+    expect(after).to.have.members([
+      ".babelrc",
+      ".editorconfig",
+      ".flowconfig",
+      ".gitignore",
+      ".travis.yml",
+      "package-lock.json",
+      "package.json",
+      "shrinkwrap.yaml",
+      "src/commands/checkout.js",
+      "src/commands/config.js",
+      "src/commands/fetch.js",
+      "src/commands/init.js",
+      "src/index.js",
+      "src/models/GitBlob.js",
+      "src/models/GitCommit.js",
+      "src/models/GitConfig.js",
+      "src/models/GitObject.js",
+      "src/models/GitTree.js",
+      "src/utils/exists.js",
+      "src/utils/mkdirs.js",
+      "src/utils/read.js",
+      "src/utils/resolveRef.js",
+      "src/utils/write.js",
+    ])
+
+    expect(before.length === after.length + 1).to.be.true
+  })
+
+  it('dir', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture(remoteDataFixture as DataFixture)
+
+    // act
+    const before = await listFiles({ fs, dir })
+
+    // assert
+    expect(before).to.have.members([
+      ".babelrc",
+      ".editorconfig",
+      ".flowconfig",
+      ".gitignore",
+      ".travis.yml",
+      "LICENSE.md",
+      "package-lock.json",
+      "package.json",
+      "shrinkwrap.yaml",
+      "src/commands/checkout.js",
+      "src/commands/config.js",
+      "src/commands/fetch.js",
+      "src/commands/init.js",
+      "src/index.js",
+      "src/models/GitBlob.js",
+      "src/models/GitCommit.js",
+      "src/models/GitConfig.js",
+      "src/models/GitObject.js",
+      "src/models/GitTree.js",
+      "src/utils/exists.js",
+      "src/utils/mkdirs.js",
+      "src/utils/read.js",
+      "src/utils/resolveRef.js",
+      "src/utils/write.js",
+    ])
+
+    // act
+    await remove({ fs, dir, filepath: 'src/models' })
+    const after = await listFiles({ fs, dir })
+
+    // assert
+    expect(after).to.have.members([
+      ".babelrc",
+      ".editorconfig",
+      ".flowconfig",
+      ".gitignore",
+      ".travis.yml",
+      "LICENSE.md",
+      "package-lock.json",
+      "package.json",
+      "shrinkwrap.yaml",
+      "src/commands/checkout.js",
+      "src/commands/config.js",
+      "src/commands/fetch.js",
+      "src/commands/init.js",
+      "src/index.js",
+      "src/utils/exists.js",
+      "src/utils/mkdirs.js",
+      "src/utils/read.js",
+      "src/utils/resolveRef.js",
+      "src/utils/write.js",
+    ])
+
+    expect(before.length === after.length + 5).to.be.true
+  })
+})
