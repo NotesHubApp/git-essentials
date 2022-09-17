fb99a0c36978e486c79122476a9ae61bc0de8998
Alex Titarenko <alx.titarenko@gmail.com>
2022-09-17 16:36:22 -0700

    add unit tests for resolveRef api

---
+++ src/api/resolveRef.ts
@@ -0,0 +1,58 @@
+// @ts-check
+import { GitRefManager } from '../managers/GitRefManager'
+import { FsClient } from '../models'
+import { FileSystem } from '../models/FileSystem'
+import { assertParameter } from '../utils/assertParameter'
+import { join } from '../utils/join'
+
+
+type ResolveRefParams = {
+  /** A file system client. */
+  fs: FsClient
+
+  /** The working tree directory path. */
+  dir: string
+
+  /** The git directory path (default: `join(dir, '.git')`). */
+  gitdir?: string
+
+  /** The ref to resolve. */
+  ref: string
+
+  /** How many symbolic references to follow before returning. */
+  depth?: number
+}
+
+/**
+ * Get the value of a symbolic ref or resolve a ref to its SHA-1 object id.
+ *
+ * @param {ResolveRefParams} args
+ *
+ * @returns {Promise<string>} Resolves successfully with a SHA-1 object id or the value of a symbolic ref
+ *
+ * @example
+ * let currentCommit = await git.resolveRef({ fs, dir: '/tutorial', ref: 'HEAD' })
+ * console.log(currentCommit)
+ * let currentBranch = await git.resolveRef({ fs, dir: '/tutorial', ref: 'HEAD', depth: 2 })
+ * console.log(currentBranch)
+ *
+ */
+export async function resolveRef(
+  { fs, dir, gitdir = join(dir, '.git'), ref, depth }: ResolveRefParams): Promise<string> {
+  try {
+    assertParameter('fs', fs)
+    assertParameter('gitdir', gitdir)
+    assertParameter('ref', ref)
+
+    const oid = await GitRefManager.resolve({
+      fs: new FileSystem(fs),
+      gitdir,
+      ref,
+      depth,
+    })
+    return oid
+  } catch (err: any) {
+    err.caller = 'git.resolveRef'
+    throw err
+  }
+}
---
+++ tests/resolveRef.test.ts
@@ -0,0 +1,103 @@
+import { expect } from 'chai'
+
+import { resolveRef } from '../src'
+import { makeFsFixture, DataFixture } from './helpers/makeFsFixture'
+
+import resolveRefDataFixture from './fixtures/data/resolveRef.json'
+
+
+describe('resolveRef', () => {
+  it('1e40fdfba1cf17f3c9f9f3d6b392b1865e5147b9', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture(resolveRefDataFixture as DataFixture)
+
+    // act
+    const ref = await resolveRef({ fs, dir, ref: '1e40fdfba1cf17f3c9f9f3d6b392b1865e5147b9' })
+
+    // assert
+    expect(ref).to.eq('1e40fdfba1cf17f3c9f9f3d6b392b1865e5147b9')
+  })
+
+  it('test-branch', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture(resolveRefDataFixture as DataFixture)
+
+    // act
+    const ref = await resolveRef({ fs, dir, ref: 'origin/test-branch' })
+
+    // assert
+    expect(ref).to.eq('e10ebb90d03eaacca84de1af0a59b444232da99e')
+  })
+
+  it('config', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture(resolveRefDataFixture as DataFixture)
+
+    // act
+    const ref = await resolveRef({ fs, dir, ref: 'config' })
+
+    // assert
+    expect(ref).to.eq('e10ebb90d03eaacca84de1af0a59b444232da99e')
+  })
+
+  it('test-tag', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture(resolveRefDataFixture as DataFixture)
+
+    // act
+    const ref = await resolveRef({ fs, dir, ref: 'test-tag' })
+
+    // assert
+    expect(ref).to.eq('1e40fdfba1cf17f3c9f9f3d6b392b1865e5147b9')
+  })
+
+  it('HEAD', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture(resolveRefDataFixture as DataFixture)
+
+    // act
+    const ref = await resolveRef({ fs, dir, ref: 'HEAD' })
+
+    // assert
+    expect(ref).to.eq('033417ae18b174f078f2f44232cb7a374f4c60ce')
+  })
+
+  it('HEAD depth', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture(resolveRefDataFixture as DataFixture)
+
+    // act
+    const ref = await resolveRef({ fs, dir, ref: 'HEAD', depth: 2 })
+
+    // assert
+    expect(ref).to.eq('refs/heads/main')
+  })
+
+  it('packed-refs', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture(resolveRefDataFixture as DataFixture)
+
+    // act
+    const ref = await resolveRef({ fs, dir, ref: 'v0.0.1' })
+
+    // assert
+    expect(ref).to.eq('1a2149e96a9767b281a8f10fd014835322da2d14')
+  })
+
+  it('non-existant refs', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture(resolveRefDataFixture as DataFixture)
+
+    // act
+    let error: { message?: string, caller?: string } = {}
+    try {
+      await resolveRef({ fs, dir, ref: 'this-is-not-a-ref' })
+    } catch (err: any) {
+      error = err
+    }
+
+    // assert
+    expect(error.message).not.to.be.undefined
+    expect(error.caller).to.eq('git.resolveRef')
+  })
+})
