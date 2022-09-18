fa49418faf346dbeec49be597fe87ee69f30c6d1
Alex Titarenko <alx.titarenko@gmail.com>
2022-09-17 18:24:11 -0700

    format code

--- tests/merge.test.ts
+++ tests/merge.test.ts
@@ -9,194 +9,105 @@ describe('merge', () => {
   it('merge main into main', async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
+    const desiredOid = await resolveRef({ fs, dir, ref: 'main' })
 
     // act
-    const desiredOid = await resolveRef({
-      fs,
-      dir,
-      ref: 'main',
-    })
-    const m = await merge({
-      fs,
-      dir,
-      ours: 'main',
-      theirs: 'main',
-      fastForwardOnly: true
-    })
+    const m = await merge({ fs, dir, ours: 'main', theirs: 'main', fastForwardOnly: true })
+
+    // assert
     expect(m.oid).to.eq(desiredOid)
     expect(m.alreadyMerged).to.be.true
     expect(m.fastForward).to.be.oneOf([false, undefined])
-    const oid = await resolveRef({
-      fs,
-      dir,
-      ref: 'main',
-    })
+    const oid = await resolveRef({ fs, dir, ref: 'main' })
     expect(oid).to.eq(desiredOid)
   })
 
   it('merge medium into main', async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
+    const desiredOid = await resolveRef({ fs, dir, ref: 'medium' })
 
     // act
-    const desiredOid = await resolveRef({
-      fs,
-      dir,
-      ref: 'medium',
-    })
-    const m = await merge({
-      fs,
-      dir,
-      ours: 'main',
-      theirs: 'medium',
-      fastForwardOnly: true,
-    })
+    const m = await merge({ fs, dir, ours: 'main', theirs: 'medium', fastForwardOnly: true })
+
+    // assert
     expect(m.oid).to.eq(desiredOid)
     expect(m.alreadyMerged).to.be.true
     expect(m.fastForward).to.be.oneOf([false, undefined])
-    const oid = await resolveRef({
-      fs,
-      dir,
-      ref: 'main',
-    })
+    const oid = await resolveRef({ fs, dir, ref: 'main' })
     expect(oid).to.eq(desiredOid)
   })
 
   it('merge oldest into main', async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
+    const desiredOid = await resolveRef({ fs, dir, ref: 'main' })
 
     // act
-    const desiredOid = await resolveRef({
-      fs,
-      dir,
-      ref: 'main',
-    })
-    const m = await merge({
-      fs,
-      dir,
-      ours: 'main',
-      theirs: 'oldest',
-      fastForwardOnly: true,
-    })
+    const m = await merge({ fs, dir, ours: 'main', theirs: 'oldest', fastForwardOnly: true })
+
+    // assert
     expect(m.oid).to.eq(desiredOid)
     expect(m.alreadyMerged).to.be.true
     expect(m.fastForward).to.be.oneOf([false, undefined])
-    const oid = await resolveRef({
-      fs,
-      dir,
-      ref: 'main',
-    })
+    const oid = await resolveRef({ fs, dir, ref: 'main' })
     expect(oid).to.eq(desiredOid)
   })
 
   it('merge newest into main', async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
+    const desiredOid = await resolveRef({ fs, dir, ref: 'newest' })
 
     // act
-    const desiredOid = await resolveRef({
-      fs,
-      dir,
-      ref: 'newest',
-    })
-    const m = await merge({
-      fs,
-      dir,
-      ours: 'main',
-      theirs: 'newest',
-      fastForwardOnly: true,
-    })
+    const m = await merge({ fs, dir, ours: 'main', theirs: 'newest', fastForwardOnly: true })
+
+    // assert
     expect(m.oid).to.eq(desiredOid)
     expect(m.alreadyMerged).to.be.oneOf([false, undefined])
     expect(m.fastForward).to.be.true
-    const oid = await resolveRef({
-      fs,
-      dir,
-      ref: 'main',
-    })
+    const oid = await resolveRef({ fs, dir, ref: 'main' })
     expect(oid).to.eq(desiredOid)
   })
 
   it('merge newest into main --dryRun (no author needed since fastForward)', async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
+    const originalOid = await resolveRef({ fs, dir, ref: 'main' })
+    const desiredOid = await resolveRef({ fs, dir, ref: 'newest' })
 
     // act
-    const originalOid = await resolveRef({
-      fs,
-      dir,
-      ref: 'main',
-    })
-    const desiredOid = await resolveRef({
-      fs,
-      dir,
-      ref: 'newest',
-    })
-    const m = await merge({
-      fs,
-      dir,
-      ours: 'main',
-      theirs: 'newest',
-      fastForwardOnly: true,
-      dryRun: true,
-    })
+    const m = await merge({ fs, dir, ours: 'main', theirs: 'newest', fastForwardOnly: true, dryRun: true })
+
+    // assert
     expect(m.oid).to.eq(desiredOid)
     expect(m.alreadyMerged).to.be.oneOf([false, undefined])
     expect(m.fastForward).to.be.true
-    const oid = await resolveRef({
-      fs,
-      dir,
-      ref: 'main',
-    })
+    const oid = await resolveRef({ fs, dir, ref: 'main' })
     expect(oid).to.eq(originalOid)
   })
 
   it('merge newest into main --noUpdateBranch', async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
+    const originalOid = await resolveRef({ fs, dir, ref: 'main' })
+    const desiredOid = await resolveRef({ fs, dir, ref: 'newest' })
 
     // act
-    const originalOid = await resolveRef({
-      fs,
-      dir,
-      ref: 'main',
-    })
-    const desiredOid = await resolveRef({
-      fs,
-      dir,
-      ref: 'newest',
-    })
-    const m = await merge({
-      fs,
-      dir,
-      ours: 'main',
-      theirs: 'newest',
-      fastForwardOnly: true,
-      dryRun: true,
-    })
+    const m = await merge({ fs, dir, ours: 'main', theirs: 'newest', fastForwardOnly: true, dryRun: true })
+
+    // assert
     expect(m.oid).to.eq(desiredOid)
     expect(m.alreadyMerged).to.be.oneOf([false, undefined])
     expect(m.fastForward).to.be.true
-    const oid = await resolveRef({
-      fs,
-      dir,
-      ref: 'main',
-    })
+    const oid = await resolveRef({ fs, dir, ref: 'main' })
     expect(oid).to.eq(originalOid)
   })
 
   it("merge 'add-files' and 'remove-files'", async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
-    const commit = (
-      await log({
-        fs,
-        dir,
-        depth: 1,
-        ref: 'add-files-merge-remove-files',
-      })
-    )[0].commit
+    const commit = (await log({ fs, dir, depth: 1, ref: 'add-files-merge-remove-files', }))[0].commit
 
     // act
     const report = await merge({
@@ -209,16 +120,11 @@ describe('merge', () => {
         email: 'mrtest@example.com',
         timestamp: 1262356920,
         timezoneOffset: -0,
-      },
+      }
     })
-    const mergeCommit = (
-      await log({
-        fs,
-        dir,
-        ref: 'add-files',
-        depth: 1,
-      })
-    )[0].commit
+
+    // assert
+    const mergeCommit = (await log({fs, dir, ref: 'add-files', depth: 1, }))[0].commit
     expect(report.tree).to.eq(commit.tree)
     expect(mergeCommit.tree).to.eq(commit.tree)
     expect(mergeCommit.message).to.eq(commit.message)
@@ -228,16 +134,9 @@ describe('merge', () => {
   it("merge 'remove-files' and 'add-files'", async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
-    const commit = (
-      await log({
-        fs,
-        dir,
-        depth: 1,
-        ref: 'remove-files-merge-add-files',
-      })
-    )[0].commit
+    const commit = (await log({ fs, dir, depth: 1, ref: 'remove-files-merge-add-files', }))[0].commit
 
-    // actTest
+    // act
     const report = await merge({
       fs,
       dir,
@@ -248,16 +147,11 @@ describe('merge', () => {
         email: 'mrtest@example.com',
         timestamp: 1262356920,
         timezoneOffset: -0,
-      },
+      }
     })
-    const mergeCommit = (
-      await log({
-        fs,
-        dir,
-        ref: 'remove-files',
-        depth: 1,
-      })
-    )[0].commit
+
+    // assert
+    const mergeCommit = (await log({ fs, dir, ref: 'remove-files', depth: 1, }))[0].commit
     expect(report.tree).to.eq(commit.tree)
     expect(mergeCommit.tree).to.eq(commit.tree)
     expect(mergeCommit.message).to.eq(commit.message)
@@ -271,16 +165,12 @@ describe('merge', () => {
     // act
     let error = null
     try {
-      await merge({
-        fs,
-        dir,
-        ours: 'delete-first-half',
-        theirs: 'delete-second-half',
-        dryRun: true,
-      })
+      await merge({ fs, dir, ours: 'delete-first-half', theirs: 'delete-second-half', dryRun: true })
     } catch (e: any) {
       error = e
     }
+
+    // assert
     expect(error).not.to.be.null
     expect(error.code).to.eq(Errors.MissingNameError.code)
   })
@@ -288,22 +178,8 @@ describe('merge', () => {
   it("merge 'delete-first-half' and 'delete-second-half' (dryRun)", async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
-    const commit = (
-      await log({
-        fs,
-        dir,
-        depth: 1,
-        ref: 'delete-first-half-merge-delete-second-half',
-      })
-    )[0]
-    const originalCommit = (
-      await log({
-        fs,
-        dir,
-        ref: 'delete-first-half',
-        depth: 1,
-      })
-    )[0]
+    const commit = (await log({ fs, dir, depth: 1, ref: 'delete-first-half-merge-delete-second-half', }))[0]
+    const originalCommit = (await log({ fs, dir, ref: 'delete-first-half', depth: 1 }))[0]
 
     // act
     const report = await merge({
@@ -315,49 +191,26 @@ describe('merge', () => {
         name: 'Mr. Test',
         email: 'mrtest@example.com',
         timestamp: 1262356920,
-        timezoneOffset: -0,
+        timezoneOffset: -0
       },
-      dryRun: true,
+      dryRun: true
     })
+
+    // assert
     expect(report.tree).to.eq(commit.commit.tree)
     // make sure branch hasn't been moved
-    const notMergeCommit = (
-      await log({
-        fs,
-        dir,
-        ref: 'delete-first-half',
-        depth: 1,
-      })
-    )[0]
+    const notMergeCommit = (await log({ fs, dir, ref: 'delete-first-half', depth: 1 }))[0]
     expect(notMergeCommit.oid).to.eq(originalCommit.oid)
     if (!report.oid) throw new Error('type error')
     // make sure no commit object was created
-    expect(
-      await fs.exists(
-        `${dir}/.git/objects/${report.oid.slice(0, 2)}/${report.oid.slice(2)}`
-      )
-    ).to.be.false
+    expect(await fs.exists(`${dir}/.git/objects/${report.oid.slice(0, 2)}/${report.oid.slice(2)}`)).to.be.false
   })
 
   it("merge 'delete-first-half' and 'delete-second-half' (noUpdateBranch)", async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
-    const commit = (
-      await log({
-        fs,
-        dir,
-        depth: 1,
-        ref: 'delete-first-half-merge-delete-second-half',
-      })
-    )[0]
-    const originalCommit = (
-      await log({
-        fs,
-        dir,
-        ref: 'delete-first-half',
-        depth: 1,
-      })
-    )[0]
+    const commit = (await log({ fs, dir, depth: 1, ref: 'delete-first-half-merge-delete-second-half' }))[0]
+    const originalCommit = (await log({ fs, dir, ref: 'delete-first-half', depth: 1 }))[0]
 
     // act
     const report = await merge({
@@ -369,41 +222,25 @@ describe('merge', () => {
         name: 'Mr. Test',
         email: 'mrtest@example.com',
         timestamp: 1262356920,
-        timezoneOffset: -0,
+        timezoneOffset: -0
       },
-      noUpdateBranch: true,
+      noUpdateBranch: true
     })
+
+    // assert
     expect(report.tree).to.eq(commit.commit.tree)
     // make sure branch hasn't been moved
-    const notMergeCommit = (
-      await log({
-        fs,
-        dir,
-        ref: 'delete-first-half',
-        depth: 1,
-      })
-    )[0]
+    const notMergeCommit = (await log({ fs, dir, ref: 'delete-first-half', depth: 1 }))[0]
     expect(notMergeCommit.oid).to.eq(originalCommit.oid)
     if (!report.oid) throw new Error('type error')
     // but make sure the commit object exists
-    expect(
-      await fs.exists(
-        `${dir}/.git/objects/${report.oid.slice(0, 2)}/${report.oid.slice(2)}`
-      )
-    ).to.eq(true)
+    expect(await fs.exists(`${dir}/.git/objects/${report.oid.slice(0, 2)}/${report.oid.slice(2)}`)).to.eq(true)
   })
 
   it("merge 'delete-first-half' and 'delete-second-half'", async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
-    const commit = (
-      await log({
-        fs,
-        dir,
-        depth: 1,
-        ref: 'delete-first-half-merge-delete-second-half',
-      })
-    )[0].commit
+    const commit = (await log({ fs, dir, depth: 1, ref: 'delete-first-half-merge-delete-second-half' }))[0].commit
 
     // act
     const report = await merge({
@@ -415,17 +252,12 @@ describe('merge', () => {
         name: 'Mr. Test',
         email: 'mrtest@example.com',
         timestamp: 1262356920,
-        timezoneOffset: -0,
-      },
+        timezoneOffset: -0
+      }
     })
-    const mergeCommit = (
-      await log({
-        fs,
-        dir,
-        ref: 'delete-first-half',
-        depth: 1,
-      })
-    )[0].commit
+
+    // assert
+    const mergeCommit = (await log({ fs, dir, ref: 'delete-first-half', depth: 1 }))[0].commit
     expect(report.tree).to.eq(commit.tree)
     expect(mergeCommit.tree).to.eq(commit.tree)
     expect(mergeCommit.message).to.eq(commit.message)
@@ -448,12 +280,14 @@ describe('merge', () => {
           name: 'Mr. Test',
           email: 'mrtest@example.com',
           timestamp: 1262356920,
-          timezoneOffset: -0,
-        },
+          timezoneOffset: -0
+        }
       })
     } catch (e: any) {
       error = e
     }
+
+    // assert
     expect(error).not.to.be.null
     expect(error.code).to.eq(Errors.MergeNotSupportedError.code)
   })
@@ -461,14 +295,7 @@ describe('merge', () => {
   it("merge two branches that modified the same file (no conflict)'", async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
-    const commit = (
-      await log({
-        fs,
-        dir,
-        depth: 1,
-        ref: 'a-merge-b',
-      })
-    )[0].commit
+    const commit = (await log({ fs, dir, depth: 1, ref: 'a-merge-b' }))[0].commit
 
     // act
     const report = await merge({
@@ -480,17 +307,12 @@ describe('merge', () => {
         name: 'Mr. Test',
         email: 'mrtest@example.com',
         timestamp: 1262356920,
-        timezoneOffset: -0,
-      },
+        timezoneOffset: -0
+      }
     })
-    const mergeCommit = (
-      await log({
-        fs,
-        dir,
-        ref: 'a',
-        depth: 1,
-      })
-    )[0].commit
+
+    // assert
+    const mergeCommit = (await log({ fs, dir, ref: 'a', depth: 1 }))[0].commit
     expect(report.tree).to.eq(commit.tree)
     expect(mergeCommit.tree).to.eq(commit.tree)
     expect(mergeCommit.message).to.eq(commit.message)
@@ -500,14 +322,7 @@ describe('merge', () => {
   it("merge two branches where one modified file and the other modified file mode'", async () => {
     // arrange
     const { fs, dir } = await makeFsFixture(mergeDataFixture as DataFixture)
-    const commit = (
-      await log({
-        fs,
-        dir,
-        depth: 1,
-        ref: 'a-merge-d',
-      })
-    )[0].commit
+    const commit = (await log({ fs, dir, depth: 1, ref: 'a-merge-d' }))[0].commit
 
     // act
     const report = await merge({
@@ -519,17 +334,12 @@ describe('merge', () => {
         name: 'Mr. Test',
         email: 'mrtest@example.com',
         timestamp: 1262356920,
-        timezoneOffset: -0,
-      },
+        timezoneOffset: -0
+      }
     })
-    const mergeCommit = (
-      await log({
-        fs,
-        dir,
-        ref: 'a',
-        depth: 1,
-      })
-    )[0].commit
+
+    // assert
+    const mergeCommit = (await log({ fs, dir, ref: 'a', depth: 1, }))[0].commit
     expect(report.tree).to.eq(commit.tree)
     expect(mergeCommit.tree).to.eq(commit.tree)
     expect(mergeCommit.message).to.eq(commit.message)
@@ -552,12 +362,14 @@ describe('merge', () => {
           name: 'Mr. Test',
           email: 'mrtest@example.com',
           timestamp: 1262356920,
-          timezoneOffset: -0,
-        },
+          timezoneOffset: -0
+        }
       })
     } catch (e: any) {
       error = e
     }
+
+    // assert
     expect(error).not.to.be.null
     expect(error.code).to.eq(Errors.MergeNotSupportedError.code)
   })
