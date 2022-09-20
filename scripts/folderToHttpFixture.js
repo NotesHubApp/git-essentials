844769753b9bdc0821de9d31b56a9a82bbcc47f6
Alex Titarenko <alx.titarenko@gmail.com>
2022-09-20 09:57:53 -0700

    update folderToHttpFixture script

--- scripts/folderToHttpFixture.js
+++ scripts/folderToHttpFixture.js
@@ -3,14 +3,28 @@ var fs = require('fs')
 var path = require('path')
 
 const scriptArgs = process.argv.slice(2)
-const [service, info, gitdir] = scriptArgs
+const [url, payload] = scriptArgs
+
+const parsedUrl = new URL(url)
+const infoRequest = parsedUrl.pathname.endsWith('/info/refs')
+
+const service = infoRequest ?
+  parsedUrl.searchParams.get('service') :
+  parsedUrl.pathname.substring(parsedUrl.pathname.lastIndexOf('/') + 1)
 
 const fixture = []
 
 const args = ['--stateless-rpc' ]
-if (info) { // TODO: Need actual check here
+if (infoRequest) {
   args.push('--advertise-refs')
 }
 args.push(gitdir)
 
 const ps = spawn(service, args, { env })
+ps.stdout.on('data', (data) => {
+  console.log('child:: '+String(data))
+})
+
+if (payload) {
+  ps.stdin.write(Buffer.from(payload, 'base64'))
+}
