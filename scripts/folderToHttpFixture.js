a656c73d2b07827ba2ccd4ceefc93e21d13e4706
Alex Titarenko <alx.titarenko@gmail.com>
2022-09-20 21:05:36 -0700

    implement folderToHttpFixture script

--- scripts/folderToHttpFixture.js
+++ scripts/folderToHttpFixture.js
@@ -1,28 +1,74 @@
 const { spawn } = require('child_process')
-const fs = require('fs')
-const path = require('path')
 
 const scriptArgs = process.argv.slice(2)
 const [url, payload] = scriptArgs
 
 const parsedUrl = new URL(url)
 const infoRequest = parsedUrl.pathname.endsWith('/info/refs')
-
+const method = infoRequest ? 'GET' : 'POST'
 const service = infoRequest ?
   parsedUrl.searchParams.get('service') :
   parsedUrl.pathname.substring(parsedUrl.pathname.lastIndexOf('/') + 1)
 
-const args = ['--stateless-rpc' ]
+const repoName = parsedUrl.pathname.split('/')[1]
+const gitdir = `tests/fixtures/remotes/${repoName}`
+
+const args = ['--stateless-rpc', '--strict']
 if (infoRequest) {
   args.push('--advertise-refs')
 }
 args.push(gitdir)
 
-const ps = spawn(service, args)
-ps.stdout.on('data', (data) => {
-  console.log('child:: '+ String(data))
+
+const gitServiceProcess = spawn(service, args)
+gitServiceProcess.stderr.on('data', (error) => {
+  throw new Error(error)
+})
+
+const chanks = []
+gitServiceProcess.stdout.on('data', (data) => {
+  chanks.push(data)
+})
+
+gitServiceProcess.on('close', () => {
+  const response = Buffer.concat(chanks)
+  const fixture = generateFixture(response)
+
+  const jsonFixture = JSON.stringify(fixture, null, 2)
+
+  console.log('Copy generated fixture below into your HttpFixure file:\n')
+  console.log(jsonFixture)
 })
 
 if (payload) {
-  ps.stdin.write(Buffer.from(payload, 'base64'))
+  gitServiceProcess.stdin.write(Buffer.from(payload, 'base64'))
+}
+
+
+/**
+ * @param {Buffer} responseBody
+ */
+ function generateFixture(responseBody) {
+  const requestContentType = !infoRequest ? { contentType: `application/x-${service}-request` } : {}
+  let requestBody = {}
+  if (payload) {
+    const requestEncoding = service !== 'git-receive-pack' ? 'utf8' : 'base64'
+    requestBody = { encoding: requestEncoding, body: Buffer.from(payload, 'base64').toString(requestEncoding) }
+  }
+  const responseEncoding = infoRequest ? 'utf8' : 'base64'
+  const responseContentType = `application/x-${service}-${ infoRequest ? 'advertisement' : 'result' }`
+
+  return {
+    request: {
+      url: url,
+      method: method,
+      ...requestContentType,
+      ...requestBody
+    },
+    response: {
+      contentType: responseContentType,
+      encoding: responseEncoding,
+      body: responseBody.toString(responseEncoding)
+    }
+  }
 }
--- tests/helpers/NoMatchingRequestFoundError.ts
+++ tests/helpers/NoMatchingRequestFoundError.ts
@@ -1,19 +1,38 @@
 import { Buffer } from 'buffer'
 
+type Command = {
+  description: string
+  command: string
+}
 
-function generateCreateFixtureCommands(requestUrl: string, payload?: Buffer) {
-  let command = `node ./scripts/requestToHttpFixture.js ${requestUrl}`
+function generateCreateFixtureCommands(requestUrl: string, payload?: Buffer): Command[] {
+  let requestToHttpFixtureCommand = `node ./scripts/requestToHttpFixture.js ${requestUrl}`
   if (payload) {
-    command += ' ' + payload.toString('base64')
+    requestToHttpFixtureCommand += ' ' + payload.toString('base64')
   }
 
-  return command
+  let folderToHttpFixtureCommand = `node ./scripts/folderToHttpFixture.js ${requestUrl}`
+  if (payload) {
+    folderToHttpFixtureCommand += ' ' + payload.toString('base64')
+  }
+
+  return [
+    {
+      description: 'If you create fixture for remote public repository',
+      command: requestToHttpFixtureCommand
+    },
+    {
+      description: 'If you create fixture from local repository located in "fixtures/remotes/*"',
+      command: folderToHttpFixtureCommand
+    }
+  ]
 }
 
 export class NoMatchingRequestFoundError extends Error {
   constructor(public readonly requestUrl: string, public readonly payload?: Buffer) {
-    const command = generateCreateFixtureCommands(requestUrl, payload)
-    const message = `No matching request found. Execute the following command to generate the fixture:\n\n${command}`
+    const commands = generateCreateFixtureCommands(requestUrl, payload)
+    const commandsText = commands.map(x => `[${x.description}]\n${x.command}`).join('\n\n')
+    const message = `No matching request found. Execute the following command to generate the fixture:\n\n${commandsText}`
 
     super(message)
   }
