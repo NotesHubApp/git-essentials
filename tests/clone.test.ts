2154928ffd41d58860cbd3d002f2c4d14029e3c2
Alex Titarenko <alx.titarenko@gmail.com>
2022-09-20 22:21:17 -0700

    add more tests for clone api

--- tests/clone.test.ts
+++ tests/clone.test.ts
@@ -26,6 +26,60 @@ describe('clone', () => {
 
     // assert
     expect(await fs.exists(`${dir}/.git`)).to.be.true
+    expect(await fs.exists(`${dir}/.git/objects`)).to.be.true
+    expect(await fs.exists(`${dir}/.git/refs/remotes/origin/main`)).to.be.true
+    expect(await fs.exists(`${dir}/.git/refs/heads/main`)).to.be.true
+    expect(await fs.exists(`${dir}/Welcome note.md`)).to.be.true
+  })
+
+  it('clone with noCheckout', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture()
+    const http = makeHttpFixture(cloneHttpFixtureData as HttpFixtureData)
+    const url = 'https://github.com/NotesHubApp/Welcome.git'
+
+    // act
+    await clone({
+      fs,
+      http,
+      dir,
+      depth: 1,
+      noTags: true,
+      singleBranch: true,
+      noCheckout: true,
+      url
+    })
+
+    // assert
+    expect(await fs.exists(`${dir}`)).to.be.true
+    expect(await fs.exists(`${dir}/.git/objects`)).to.be.true
+    expect(await fs.exists(`${dir}/.git/refs/remotes/origin/main`)).to.be.true
+    expect(await fs.exists(`${dir}/.git/refs/heads/main`)).to.be.true
+    expect(await fs.exists(`${dir}/Welcome note.md`)).to.be.false
+  })
+
+  it('clone a tag', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture()
+    const http = makeHttpFixture(cloneHttpFixtureData as HttpFixtureData)
+
+    // act
+    await clone({
+      fs,
+      http,
+      dir,
+      depth: 1,
+      singleBranch: true,
+      ref: 'v2.0',
+      url: 'https://github.com/NotesHubApp/Welcome.git'
+    })
+
+    // assert
+    expect(await fs.exists(`${dir}`)).to.be.true
+    expect(await fs.exists(`${dir}/.git/objects`)).to.be.true
+    expect(await fs.exists(`${dir}/.git/refs/remotes/origin/v2.0`)).to.be.false
+    expect(await fs.exists(`${dir}/.git/refs/heads/v2.0`)).to.be.false
+    expect(await fs.exists(`${dir}/.git/refs/tags/v2.0`)).to.be.true
     expect(await fs.exists(`${dir}/Welcome note.md`)).to.be.true
   })
 
@@ -46,6 +100,33 @@ describe('clone', () => {
 
     // assert
     expect(await currentBranch({ fs, dir })).to.eq('i-am-not-master')
+  })
+
+  it('clone with an unregistered protocol', async () => {
+    // arrange
+    const { fs, dir } = await makeFsFixture()
+    const http = makeHttpFixture(cloneHttpFixtureData as HttpFixtureData)
+    const url = `foobar://github.com/NotesHubApp/Welcome`
+    let error = null
+
+    // act
+    try {
+      await clone({
+        fs,
+        http,
+        dir,
+        depth: 1,
+        singleBranch: true,
+        ref: 'test-tag',
+        url,
+      })
+    } catch (err: any) {
+      error = err
+    }
+
+    // assert
+    expect(error.message).to.eq(`Git remote "${url}" uses an unrecognized transport protocol: "foobar"`)
+    expect(error.caller).to.eq('git.clone')
   })
 
   // it('clone empty repository', async () => {
