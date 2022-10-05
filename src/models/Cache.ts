5eca6776e5859bcecd0553117a813a9adf6f2679
Alex Titarenko <alx.titarenko@gmail.com>
2022-10-05 09:52:42 -0700

    code refactoring

--- src/models/Cache.ts
+++ src/models/Cache.ts
@@ -1,11 +1,28 @@
 import { Stat } from './FsClient'
 
+/**
+ * @group Cache
+ */
 export const IndexCache = Symbol('IndexCache')
+
+/**
+ * @group Cache
+ */
 export const PackfileCache = Symbol('PackfileCache')
 
+/**
+ * @group Cache
+ */
 export type GitPackIndex = object
+
+/**
+ * @group Cache
+ */
 export type GitIndex = object
 
+/**
+ * @group Cache
+ */
 export type Cache = {
   [IndexCache]?: { map: Map<string, GitIndex>, stats: Map<string, Stat | null>}
   [PackfileCache]?: Map<string, Promise<GitPackIndex | undefined>>
