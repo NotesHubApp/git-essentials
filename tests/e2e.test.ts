c5d0fdd3ef19e4e39005edb1236da796ecaa900c
Alex Titarenko <alx.titarenko@gmail.com>
2022-10-20 18:27:09 -0700

    test(init): add e2e init test

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,9 +1,22 @@
-import { clone } from 'git-essentials'
+import { init, clone } from 'git-essentials'
 
 import { integrationContext } from './helpers/integrationContext'
 
 
 describe('e2e', () => {
+  describe('init', () => {
+    it('should init new repository', async () => {
+      await integrationContext(async ({ fs, http, dir }) => {
+        // act
+        await init({ fs, dir, defaultBranch: 'main' })
+
+        // assert
+        const headFile = await fs.readFile(`${dir}/.git/HEAD`, { encoding: 'utf8' })
+        expect(headFile).toContain('refs/heads/main')
+      })
+    })
+  })
+
   describe('clone', () => {
     it('should clone from GitHub', async () => {
       await integrationContext(async ({ fs, http, dir }) => {
